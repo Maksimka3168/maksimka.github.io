75795e18d0a0e82be65afdb03b9adca11db8eef6
Maksimka3168 <gfer-6@mail.ru>
2023-01-23 01:01:44 +0300

    новые обновления

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
 const htmlMain = document.getElementById("html_main");
 const widthSize = window.screen.width
 const heightSize = window.screen.height
-htmlMain.setAttribute('style', `--tg-viewport-height:${widthSize}; --tg-viewport-stable-height:${heightSize};`);
+htmlMain.setAttribute('style', `--tg-viewport-height:${widthSize}px; --tg-viewport-stable-height:${heightSize}px;`);
