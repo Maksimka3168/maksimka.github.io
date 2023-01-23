80c05261e85be2f2f5f00b4d2a9c48e84417fac8
Maksimka3168 <gfer-6@mail.ru>
2023-01-23 15:11:33 +0300

    новые обновления

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-const htmlMain = document.getElementById("html_main");
-const widthSize = window.screen.width
-const heightSize = window.screen.height
-htmlMain.setAttribute('style', `--tg-viewport-height:774px; --tg-viewport-stable-height:774px;`);
+let tg = window.Telegram.WebApp;
+const containerElement = document.getElementById("conatiner_id");
+containerElement.style.width = tg.viewportHeight;
